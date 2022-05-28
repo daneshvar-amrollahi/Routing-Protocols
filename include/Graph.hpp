5f97ae5471630f5fcd776383588b39137dd960cb
kymmt80 <kimiya80@gmail.com>
2022-05-28 13:51:31 +0430

    add base classes

---
+++ include/Graph.hpp
@@ -0,0 +1,13 @@
+#ifndef __GRAPH_HPP__
+#define __GRAPH_HPP__
+
+class Graph
+{
+private:
+    /* data */
+public:
+    Graph(/* args */);
+    ~Graph();
+};
+
+#endif
---
+++ include/Network.hpp
@@ -0,0 +1,13 @@
+#ifndef __NETWORK_HPP__
+#define __NETWORK_HPP__ 
+
+class Network
+{
+private:
+    /* data */
+public:
+    Network(/* args */);
+    void run();
+};
+
+#endif
